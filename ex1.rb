5d0516564ab76207f7ea53d09a375729bb228aa6
Dawid <d.kulpa@selleo.com>
2016-08-02 14:37:28 +0200

    Create a superclass called Vehicle for your MyCar class to inherit from and move the behavior that isn't specific to the MyCar class to the superclass. Create a constant in your MyCar class that stores information about the vehicle that makes it different from other types of Vehicles.
    Then create a new class called MyTruck that inherits from your superclass that also has a constant defined that separates it from the MyCar class in some way.

--- ex1.rb
+++ ex1.rb
@@ -1,16 +1,42 @@
-class Animal
-  attr_accessor :name
+class Vehicle
+  attr_accessor :speed, :color, :year
 
-  def initialize(name)
-    @name = name
-  end
-end
-
-class GoodDog < Animal
-  def initialize(color)
-    super
+  def initialize(year, color)
+    @year = year
     @color = color
+    @speed = 0
+  end
+
+  def speed_up(kph)
+    @speed =+ kph
+  end
+
+end
+
+class MyCar < Vehicle
+  IS_DAMAGED = true
+
+  def initialize(model, engine)
+    @model = model
+    @engine = engine
+  end
+
+  def describe
+    puts "Your car (damaged? #{IS_DAMAGED}) is #{@color} #{@model} from #{@year} and it has speed #{speed} km/h"
   end
 end
 
-bruno = GoodDog.new('brown')
+class MyTruck < Vehicle
+  IS_DAMAGED = false
+
+  def initialize(truck)
+    @truck = truck
+  end
+end
+
+arrow = MyCar.new('Peugeot', '1.4 TDI')
+
+arrow.speed_up(30)
+arrow.color = 'silver'
+arrow.year = 2014
+arrow.describe
