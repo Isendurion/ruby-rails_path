2f9aed486103a9458723e7102a87d6797199d02e
Dawid <d.kulpa@selleo.com>
2016-08-03 08:08:56 +0200

    Create a class 'Student' with attributes name and grade. Do NOT make the grade getter public, so joe.grade will raise an error. Create a better_grade_than? method, that you can call like so...

---
+++ ex2.rb
@@ -0,0 +1,22 @@
+class Student
+
+  def initialize(name, grade)
+    @name = name
+    @grade = grade
+  end
+
+  def better_grade_than?(student)
+    grade > student.grade
+  end
+
+  protected
+
+  def grade
+    @grade
+  end
+end
+
+mary = Student.new('Mary', 4)
+martha = Student.new('Martha', 2)
+#puts mary.grade
+puts mary.better_grade_than?(martha)
